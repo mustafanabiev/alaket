5f87a3c88ff4dbe751ba4a17e86cb0bea83490fb
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2023-01-14 23:19:47 +0600

    new project

---
+++ android/app/src/main/kotlin/com/example/alaket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alaket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:alaket/app/app.dart';
+import 'package:alaket/app/view/meta_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
